b2d6934640b0fc1e53e9faa1533a2e75ba91d313
menachem yarhi <nbjo460@gmail.com>
2025-06-09 11:29:04 +0300

    Added Person

---
+++ Models/Person.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Malshinon.Models
+{
+    internal class Person
+    {
+        public int ID { get; set; }
+        public string firstName { get; set; }
+        public string lastName {get; set;}
+        public string secretCode { get; set; }
+        public string type { get; set; }
+        public int numReports { get; set; }
+        public int numMentions { get; set; }
+    }
+}
