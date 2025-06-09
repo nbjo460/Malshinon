b7e8140923555b0be20f53cbc5772c40d37e29f7
menachem yarhi <nbjo460@gmail.com>
2025-06-09 11:33:11 +0300

    Added IntelReport

---
+++ Models/IntelReports.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Malshinon.Models
+{
+    internal class IntelReports
+    {
+        public int ID { get; set; }
+        public int ReporterId { get; set; }
+        public int TargetId { get; set; }
+        public string Text { get; set; }
+        public DateTime TimeStamp { get; set; }
+    }
+}
